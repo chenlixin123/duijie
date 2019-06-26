b3efef145c8f28fa5cd776bc4faf3ec5f7334f60
qlzw226 <qlzw225@sina.com>
2019-06-26 11:42:16 +0800

    测试版本

--- build/webpack.dev.conf.js
+++ build/webpack.dev.conf.js
@@ -63,7 +63,7 @@ const devWebpackConfig = merge(baseWebpackConfig, {
     },
     proxy: {
       '/api': {
-        target: 'http://api.linkmoretech.cn/', // 开发cn 测试net  线上com
+        target: 'http://api.linkmoreparking.net/', // 开发cn 测试net  线上com
         pathRewrite: {
           '^/api': 'api/'
         },
